2641625bbe7433563d7d57c71f1b869d057f61ce
Stefano Rosa <rosa.stefano@gmail.com>
2025-09-17 19:16:06 +0200

    Add files via upload

---
+++ dreamsim_localization.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import csv
+import numpy as np
+import pandas as pd
+import rclpy
+import sys
+import torch.nn.functional as F
+
+from cv_bridge import CvBridge
+from dreamsim import dreamsim
+from PIL import Image as PILImage
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from tqdm import tqdm
+
+
+def pose_to_transformation_matrix(x, y, yaw):
+    """
+    Converts a 2D pose (x, y, yaw) into a 3x3 homogeneous transformation matrix.
+
+    Parameters:
+    - x (float): X position
+    - y (float): Y position
+    - yaw (float): Rotation angle in radians
+
+    Returns:
+    - np.ndarray: 3x3 transformation matrix
+    """
+    cos_yaw = np.cos(yaw)
+    sin_yaw = np.sin(yaw)
+
+    T = np.array([
+        [cos_yaw, -sin_yaw, x],
+        [sin_yaw,  cos_yaw, y],
+        [0,        0,       1]
+    ])
+
+    return T
+    
+class Localizer(Node):
+    def __init__(self):
+        super().__init__("localizer_node")
+
+        # read image-pose map
+        self.image_pose_df = pd.read_csv(sys.argv[1] + "/map.csv")
+        self.images = []
+        for filename in self.image_pose_df['filename']:
+            self.images.append(PILImage.open(sys.argv[1] + "/" + filename))
+
+        # init model
+        self.device = "cpu"
+        self.model, self.preprocess = dreamsim(pretrained=True, device=self.device)
+
+        # compute query embeddings
+        self.query_embeddings = []
+        for image in self.images:
+            self.query_embeddings.append(self.model.embed(self.preprocess(image).to(self.device)))
+        print("Done loading model and preprocessing images.")
+        self.last_rgb = None
+        self.br = CvBridge()
+                
+        self.img_sub = self.create_subscription(Image, "/camera_left/image_rect_color", self.image_callback, 10)
+        self.amcl_sub = self.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", self.amcl_callback, 10)
+        self.pose_pub = self.create_publisher(PoseStamped, "pose", 10)
+        
+    def image_callback(self, msg):
+        self.last_rgb = self.br.imgmsg_to_cv2(msg).copy()[:,:,::-1]
+
+    def amcl_callback(self, msg):
+        print("received amcl pose")
+        if msg.pose.covariance.max() > 0.15*0 and self.last_rgb is not None:
+            print("covariance too high, re-localizing...")
+
+            target_emb = self.model.embed(self.preprocess(PILImage.fromarray(self.last_rgb)).to(self.device))
+
+            similarities = []
+
+            for i, query_emb in tqdm(enumerate(self.query_embeddings), total=len(self.query_embeddings)):
+                similarity = F.cosine_similarity(query_emb, target_emb, dim=-1).item()
+                similarities.append(similarity)
+
+            est = np.argmax(similarities)
+
+            x = self.image_pose_df['x'][est]
+            y = self.image_pose_df['y'][est]
+            a = self.image_pose_df['a'][est]
+ 
+            # T = pose_to_transformation_matrix(x, y, a)
+            # Tinv = np.linalg.inv(T)
+            # x = T[0,-1]
+            # y = T[1,-1]
+             
+            print("localized at: ", x, y, a, "matched to", self.image_pose_df['filename'][est], "with similarity", similarities[est])
+
+            # TODO: send pose to amcl
+            if similarities[est] > 0.7:
+                print("sending pose to amcl")
+                pose_msg = PoseStamped()
+                pose_msg.header.frame_id = "map"
+                pose_msg.header.stamp = msg.header.stamp
+                pose_msg.pose.position.x = x
+                pose_msg.pose.position.y = y
+                pose_msg.pose.position.z = 0.0
+                self.pose_pub.publish(pose_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Localizer()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
---
+++ dreamsim_mapping.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import csv
+import cv2
+import message_filters
+import numpy as np
+import rclpy
+import sys
+import time
+
+from cv_bridge import CvBridge
+from PIL import Image as PILImage
+from rclpy.node import Node
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from sensor_msgs.msg import Image
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+
+class Mapper(Node):
+    def __init__(self):
+        super().__init__("mapper_node")
+
+        self.img_count = 0
+        self.last_time = time.time()
+        self.csvfile = open('map.csv', 'w', newline='')
+        self.spamwriter = csv.writer(self.csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        self.br = CvBridge()
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+                
+        self.img_sub = self.create_subscription(Image, "/cer/realsense_repeater/color_image", self.image_callback, 10)
+        
+        # self.img_sub = message_filters.Subscriber(self, Image, "/cer/realsense_repeater/color_image")
+        # self.amcl_sub = message_filters.Subscriber(self, PoseWithCovarianceStamped, "amcl_pose")
+        # self.tss = message_filters.ApproximateTimeSynchronizer([self.img_sub, self.amcl_sub], 1, slop=1.0)
+        # self.tss.registerCallback(self.combined_callback)
+
+    def image_callback(self, msg):
+        print("callback!")
+        current_time = time.time()
+        if current_time - self.last_time < 1.0:
+            print("too early!")
+            return
+        self.last_time = current_time
+        
+        ############# RT #################
+        T = None
+        try:
+            T = self.tf_buffer.lookup_transform(
+                    "map",
+                    "mobile_base_body_link",
+                    msg.header.stamp  #rclpy.time.Time()   #depth_msg.header.stamp 
+                )
+        except TransformException:
+            print("Could not transform!")
+            return
+
+        q = T.transform.rotation
+        x = str(T.transform.translation.x)
+        y = str(T.transform.translation.y)
+        a = str(np.arctan2(2.0 * (q.z * q.w + q.x * q.y) , - 1.0 + 2.0 * (q.w * q.w + q.x * q.x)))
+        
+        rgb = self.br.imgmsg_to_cv2(msg).copy()[:,:,::-1]
+        filename = "map_image_" + str(self.img_count).zfill(5) + ".jpg"
+        cv2.imwrite(filename, rgb)
+        self.spamwriter.writerow([str(self.img_count), x, y, a, filename])
+        self.img_count += 1
+                        
+    # def combined_callback(self, img_msg, amcl_msg):
+        # print("callback!")
+        # rgb = self.br.imgmsg_to_cv2(img_msg).copy()[:,:,::-1]
+        # pose = amcl_msg.pose.pose
+# 
+        # filename = "map_image_" + str(self.img_count).zfill(5) + ".jpg"
+        # cv2.imwrite(filename, rgb)
+        # x = str(pose.position.x)
+        # y = str(pose.position.y)
+        # q = pose.orientation
+        # a = str(np.atan2(2.0 * (q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z))
+        # 
+        # self.spamwriter.write([str(self.img_count), x, y, a, filename])
+        # self.img_count += 1
+        
+def main(args=None):
+    rclpy.init(args=args)
+    node = Mapper()
+    rclpy.spin(node)
+    node.csvfile.close()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
